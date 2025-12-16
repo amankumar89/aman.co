0fe2c5a54610ac015282c4b1283d4ff0c1abca0b
amankumar89 <amankumaroo784@gmail.com>
2025-12-16 20:20:59 +0530

    refactored and optimised protect middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,16 +1,25 @@
+import jwt from "jsonwebtoken";
+
 // JWT Auth Middleware
 export const protect = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401).json({ message: 'Not Authorized!' });
+  try {
+    const authHeader = req.headers.authorization;
+    let token;
+
+    if (authHeader?.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    } else if (req.cookies?.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Not Authorized!" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    return next();
+  } catch (err) {
+    return res.status(403).json({ message: "Not Authorized!" });
   }
 };
